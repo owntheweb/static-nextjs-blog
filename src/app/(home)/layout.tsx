92a7f95d6011f1357b53997df07e10d47063e6de
Christopher Stevens <chris.stevens@bombbomb.com>
2023-06-03 13:40:45 -0600

    parallax pondering to consider more later

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -15,23 +15,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={
-        `${inter.className}
-        bg-[url('/images/header-background-768.jpg')]
-        md:bg-[url('/images/header-background-1024.jpg')]
-        lg:bg-[url('/images/header-background-1280.jpg')]
-        xl:bg-[url('/images/header-background-1536.jpg')]
-        bg-no-repeat
-        bg-top h-full`}>
-        <HomeHeader />
-        <div className="mx-auto lg:max-w-4xl py-8 lg:rounded-md drop-shadow-2xl px-6 lg:px-0">
-          <div className="flex">
-            <SideNav className="flex-none w-48 max-sm:hidden" />
-            <div className="">
-              {children}
+        `${inter.className}`}>
+          <div className="parallaxContent bg-top">
+            <HomeHeader />
+            <div className="mx-auto lg:max-w-4xl py-8 lg:rounded-md drop-shadow-2xl px-6 lg:px-0">
+              <div className="flex">
+                <SideNav className="flex-none w-48 max-sm:hidden" />
+                <div className="">
+                  {children}
+                </div>
+              </div>
+              <Footer />
             </div>
           </div>
-          <Footer />
-        </div>
       </body>
     </html>
   )

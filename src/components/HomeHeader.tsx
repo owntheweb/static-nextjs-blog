1ffa98761d45723bfc79c9181e57c1db7d1f388e
Christopher Stevens <chris.stevens@bombbomb.com>
2023-06-04 07:24:45 -0600

    WIP: consider home page typed story, pausing as it is rather distracting

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import './HomeHeaderStyle.css';
 import TopNav from "./TopNav";
+import OriginStory from "./OriginStory";
 
 const HomeHeader = () => {  
   return (
     <header className="lg:pb-6 mb-8">
       <TopNav />
-      <div className="headerSpace"></div>
+      <div className="headerSpace">
+        <OriginStory className="headerStory" />
+      </div>
     </header>
   )
 }
